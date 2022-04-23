ac2ec34550c8afba5aa46ce7e3d2868c3e994d79
Billy Wong <bwong.yh@protonmail.com>
2022-04-24 06:07:46 +0900

    Update getData function accpets breed argument from the command-line and logs the breed description

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,11 @@
 const request = require("request");
+const breed = process.argv[2];
+
+const getData = breed => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, res, body) => {
+    const data = JSON.parse(body)[0];
+    console.log(data.description);
+  });
+};
+
+getData(breed);
