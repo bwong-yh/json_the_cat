af3ae9395f0409ca89845489a26c05e57fe057e3
Billy Wong <bwong.yh@protonmail.com>
2022-04-24 06:49:40 +0900

    Update getData function passes parsed data to callback function to be logged and handles errors if any

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,16 @@
 const request = require("request");
 const breed = process.argv[2];
 
-const getData = breed => {
+const getData = (breed, cb) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (err, res, body) => {
+    if (err) return cb(err, null);
+
     const data = JSON.parse(body)[0];
-    console.log(data.description);
+    data ? cb(null, data) : cb("Uh oh! Cannot find breed!", null);
   });
 };
 
-getData(breed);
+getData(breed, (err, data) => {
+  const { description } = data;
+  err ? console.log(err) : console.log(description);
+});
